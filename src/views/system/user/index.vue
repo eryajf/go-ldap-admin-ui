f499c06c299f1e08a741843f41cc2ced80c2e191
eryajf <eryajf@163.com>
2022-12-12 10:02:02 +0800

    fix mobile has 16

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -143,7 +143,7 @@ export default {
       if (!value) {
         return callback(new Error('手机号不能为空'))
       } else {
-        const reg = /^1[3|4|5|7|8|9][0-9]\d{8}$/
+        const reg = /^1[3|4|5|6|7|8|9][0-9]\d{8}$/
         if (reg.test(value)) {
           callback()
         } else {
